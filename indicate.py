5fd7d49d32463524acffb22520018d168e51070f
AlphaArslan <arslan.alpha21@gmail.com>
2019-10-03 16:39:31 +0200

    added test function

--- indicate.py
+++ indicate.py
@@ -1,9 +1,8 @@
-from config import *
 import RPi.GPIO as GPIO
-
+import time
 
 class StatusLed():
-    def __init__(self, (R_pin, G_pin, B_pin)):
+    def __init__(self, [R_pin, G_pin, B_pin]):
         self.rpin = R_pin
         self.gpin = G_pin
         self.bpin = B_pin
@@ -15,3 +14,10 @@ class StatusLed():
         GPIO.output(self.rpin, r)
         GPIO.output(self.gpin, g)
         GPIO.output(self.bpin, b)
+
+    def test(self):
+        for i in [0, 1]:
+            for j in [0, 1]:
+                for h in [0, 1]:
+                    self.indicate(self, (i, j, h))
+                    time.sleep(2)
