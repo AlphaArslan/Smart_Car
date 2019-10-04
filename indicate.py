933d1c7f995e8c48bfc2424d9ee0bbc6160ffdcb
AlphaArslan <arslan.alpha21@gmail.com>
2019-10-04 14:52:16 +0200

    done testing RGB led

--- indicate.py
+++ indicate.py
@@ -1,8 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
 class StatusLed():
-    def __init__(self, [R_pin, G_pin, B_pin]):
+    def __init__(self, R_pin, G_pin, B_pin):
         self.rpin = R_pin
         self.gpin = G_pin
         self.bpin = B_pin
@@ -10,7 +13,7 @@ class StatusLed():
         GPIO.setup(G_pin, GPIO.OUT)
         GPIO.setup(B_pin, GPIO.OUT)
 
-    def indicate(self, (r, g, b)):
+    def indicate(self, r, g, b):
         GPIO.output(self.rpin, r)
         GPIO.output(self.gpin, g)
         GPIO.output(self.bpin, b)
@@ -19,5 +22,8 @@ class StatusLed():
         for i in [0, 1]:
             for j in [0, 1]:
                 for h in [0, 1]:
-                    self.indicate(self, (i, j, h))
+                    self.indicate(i, j, h)
                     time.sleep(2)
+
+
+# try to pass tuples to functions
