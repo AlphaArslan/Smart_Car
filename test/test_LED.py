ed3acd9ff61d8f0615190f02dd91cf2bcc59d618
AlphaArslan <arslan.alpha21@gmail.com>
2019-10-04 15:10:24 +0200

    pass tuple

--- test/test_LED.py
+++ test/test_LED.py
@@ -6,26 +6,6 @@ import config
 import indicate
 
 ################ MAIN
-led = indicate.StatusLed(config.STATUS_LED_PIN[0], config.STATUS_LED_PIN[1], config.STATUS_LED_PIN[2])
-
-print("MAGENTA")
-led.indicate(1, 0, 1)
-time.sleep(3)
-
-print("YELLOW")
-led.indicate(1, 1, 0)
-time.sleep(3)
-
-print("CYAN")
-led.indicate(0, 1, 1)
-time.sleep(3)
-
-print("WHITE")
-led.indicate(1, 1, 1)
-time.sleep(3)
-
-
-led.indicate(0, 0, 0)
+led = indicate.StatusLed(config.STATUS_LED_PIN)
 
 led.test()
-led.indicate(0, 0, 0)
