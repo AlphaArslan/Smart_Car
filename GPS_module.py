7522e23e873eb74b36ea97403e5583ba4325d4e3
AlphaArslan <arslan.alpha21@gmail.com>
2019-10-17 12:56:37 +0200

    code skeleton for gps class

---
+++ GPS_module.py
@@ -0,0 +1,18 @@
+"""
+This module communicates with the GPS module
+Ublox Neo 6m
+https://www.instructables.com/id/Raspberry-Pi-the-Neo-6M-GPS/
+"""
+
+class GPS():
+    def __init__(self):
+        pass
+
+    def get_location(self):
+        """
+        returns (latitude, longitude)
+        """
+        latitude = 0
+        longitude = 0
+
+        return (latitude, longitude)
