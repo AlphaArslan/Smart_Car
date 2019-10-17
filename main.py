de3b76f450c3cbbb4f2779326e739f27856cc3c4
AlphaArslan <arslan.alpha21@gmail.com>
2019-10-17 12:54:55 +0200

    initial working loop

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ import zmq
 from time import sleep
 
 import config
-import motor
 import indicate
+import auto
 
+########################### obj
+status_led_obj = indicate.StatusLed(config.STATUS_LED_PIN)
+pilot_obj = auto.Pilot()
 
-########################### FUNC
-
+########################### functions
+def wait_for_tasks():
+    # listen for tasks from web
+    location = (0,0)
+    return location
 
 ########################### MAIN
 if __name__ == '__main__':
-    pass
+    while True:
+        status_led_obj.indicate(config.FREE_STATUS_COLOR)
+        loc = wait_for_tasks()
+        status_led_obj.indicate(config.BUSY_STATUS_COLOR)
+        pilot_obj.go_to_locaion(loc)
