ad656fb267c635f0277d5e26ac5d017e75e6fdfe
AlphaArslan <arslan.alpha21@gmail.com>
2019-11-04 00:23:58 +0200

    fix indentation in ultrasonic

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,4 +1,4 @@
-# tested and working 
+# tested and working
 
 import RPi.GPIO as GPIO
 import time
@@ -18,28 +18,28 @@ class UltraSonic():
         """
         returns distance in cm
         """
-    	# set Trigger to HIGH
-    	GPIO.output(self.trig, True)
-    	# set Trigger after 0.01ms to LOW
-    	time.sleep(0.00001)
-    	GPIO.output(self.trig, False)
+        # set Trigger to HIGH
+        GPIO.output(self.trig, True)
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(self.trig, False)
 
-    	startTime = time.time()
-    	stopTime = time.time()
+        startTime = time.time()
+        stopTime = time.time()
 
-    	# save start time
-    	while 0 == GPIO.input(self.echo):
-    		startTime = time.time()
+        # save start time
+        while 0 == GPIO.input(self.echo):
+        	startTime = time.time()
 
-    	# save time of arrival
-    	while 1 == GPIO.input(self.echo):
-    		stopTime = time.time()
+        # save time of arrival
+        while 1 == GPIO.input(self.echo):
+        	stopTime = time.time()
 
-    	# time difference between start and arrival
-    	TimeElapsed = stopTime - startTime
-    	# multiply with the sonic speed (34300 cm/s)
-    	# and divide by 2, because there and back
-    	distance = (TimeElapsed * 34300) / 2
+        # time difference between start and arrival
+        TimeElapsed = stopTime - startTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
         return distance
 
     def is_blocked(self, threshold):
