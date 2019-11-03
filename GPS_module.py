f65e29d4df30699a67aa81f69a84be973df55260
AlphaArslan <arslan.alpha21@gmail.com>
2019-11-03 15:57:33 +0200

    merged test + added debug

--- GPS_module.py
+++ GPS_module.py
@@ -14,15 +14,20 @@ https://www.instructables.com/id/Raspberry-Pi-the-Neo-6M-GPS/
 import serial
 import pynmea2
 import time
+import config
 
 class GPS():
-    def __init__(self):
+    def __init__(self, dbg= config.DEBUG_MODE):
+        self.dbg = dbg
         self.serial_port = serial.Serial("/dev/serial0", 9600, timeout=0.5)
 
     def get_location(self):
         """
         returns (latitude, longitude)
         """
+        if self.dbg:
+            print("[GPS] getting gps location")
+
         str = self.serial_port.readline()
         while (str.find('GGA') == 0):
             print("waitng for GPS data")
@@ -30,8 +35,14 @@ class GPS():
             str = self.serial_port.readline()
 
         msg = pynmea2.parse(str)
+
+        loc = (0, 0)
+        if self.dbg:
+            print("[GPS] gps location: {}".format(loc))
+
         return msg
-        # latitude = 0
-        # longitude = 0
-        #
-        # return (latitude, longitude)
+
+##########################################
+if __name__ == '__main__':
+    gps = GPS()
+    print(gps.get_location())
